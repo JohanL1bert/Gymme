1461184578197cb3ddf9c76026c7150e37783f37
Johan <grimmerw968@gmail.com>
2022-11-21 13:40:42 +0200

    feat: add styles to config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,24 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    colors: {
+      'primary-100': '#A5FFCE',
+      'primary-200': '#4AE290',
+      'primary-300': '#25C870',
+      'neutral-500': '#131316',
+      'neutral-400': '#595962',
+      'neutral-300': '#9797A1',
+      'neutral-200': '#DEDEE3',
+      'neutral-100': '#CFCFDE',
+    },
+    extend: {
+      spacing: {
+        SMALL: '16px',
+        MEDIUM: '32px',
+        LARGE: '80px',
+        XLARGE: '180px',
+      },
+    },
   },
   plugins: [],
 };
