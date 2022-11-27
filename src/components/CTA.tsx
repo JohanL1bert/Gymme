a3baf8edd6174814e64ac38bf1e294202927acac
Johan <grimmerw968@gmail.com>
2022-11-27 12:03:09 +0200

    feat: add right side of page

--- src/components/CTA.tsx
+++ src/components/CTA.tsx
@@ -4,15 +4,28 @@ import CTAImg from '../assets/image/CTA.png';
 export const CTA = () => {
   return (
     <section>
-      <div className="h-[calc(theme(spacing.96)+theme(spacing.40)-7px)] bg-neutral-500">
-        <div className="flex justify-center ">
+      <div className="h-[calc(theme(spacing.96)+theme(spacing.40)-7px)] bg-neutral-500 flex justify-center items-center">
+        <div className="">
           <img
             src={CTAImg}
             alt="image of CTA"
             className="h-[618px] w-[500px] z-10 -translate-y-10"
           />
         </div>
-        <div>text</div>
+        <div className="h-[290px] w-[492px]">
+          <div>
+            <h3 className="H1 text-neutral-100 text-start">
+              Wanna join
+              <span className="text-primary-200"> & </span>
+              have fun?
+            </h3>
+          </div>
+          <div className="body_medium text-neutral-100 text-start mt-6">
+            We’ll keep you updated on the things you need to know about Gymme.
+            Nothing more, nothing less.
+          </div>
+          <button className="mt-8 uppercase">Join now</button>
+        </div>
       </div>
     </section>
   );
