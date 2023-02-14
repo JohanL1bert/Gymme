8471dd0ca1bab2b4f8aaba472dc476acee6294bc
Johan <grimmerw968@gmail.com>
2023-02-14 12:30:38 +0200

    feat: add template stryles, vizualization to pricing card

--- src/components/pricing/PricingCard.tsx
+++ src/components/pricing/PricingCard.tsx
@@ -1,46 +1,51 @@
 import React from 'react';
 import { chooseCardStyle } from '../../helpers/chooseCardStyle';
 import { ICardDetails, TCardKeys } from '../pricing/pricingCard.data';
+import { TPricingCardTypography } from './pricingCard.typography';
+import classNames from 'classnames';
 
-interface ICardColorSize {
-  color: string;
-  sizeW: string;
-  sizeH: string;
-}
-
-type TCardStylish = {
-  // eslint-disable-next-line no-unused-vars
-  [key in TCardKeys]: ICardColorSize;
-};
-
-export interface ICardChooseDetails extends TCardStylish {}
+export interface ICardChooseDetails extends TPricingCardTypography {}
 export type TCardIndexSignature = TCardKeys;
 
 export const PricingCard = ({
   cardStylish,
   cardDetails,
 }: {
-  cardStylish: TCardStylish;
+  cardStylish: TPricingCardTypography;
   cardDetails: ICardDetails;
 }) => {
-  chooseCardStyle(cardDetails.styleCard, cardStylish);
+  const cardStyle = chooseCardStyle(cardDetails.styleCard, cardStylish);
+
+  const upperBtnStyle = Object.values({ ...cardStyle.upperBtnStyle });
+  const { priceColor, usdColor, monthColor } = cardStyle.priceSection;
 
   return (
-    <div className="w-[calc(theme(spacing.96)+21px)]">
-      <div>{cardDetails.heading}</div>
-      <div>
-        <div>
-          <div>{cardDetails.price}</div>
-          <div>$</div>
+    <div className={`${cardStyle.sizeH} ${cardStyle.sizeW} ${cardStyle.color}`}>
+      <div className="flex flex-col items-center px-24">
+        <div className={classNames(upperBtnStyle, 'rounded text-center')}>
+          {cardDetails.heading}
         </div>
-        <div>/month</div>
+        <div>
+          <div className="flex flex-col items-center">
+            <div className={priceColor}>
+              {cardDetails.price}
+              <span className={usdColor}>$</span>
+            </div>
+          </div>
+          <div className={monthColor}>/month</div>
+        </div>
+        <ul className="flex flex-col gap-y-4">
+          {cardDetails.services.map((item, index) => (
+            <li key={index}>
+              <span className="w-[20px] h-[20px] rounded-full">
+                <span className="" />
+              </span>
+              <span className={cardStyle.textColor}>{item.features}</span>
+            </li>
+          ))}
+        </ul>
+        <button className="">join now</button>
       </div>
-      <ul>
-        {cardDetails.services.map((item, index) => (
-          <li key={index}>{item.features}</li>
-        ))}
-      </ul>
-      <button>join now</button>
     </div>
   );
 };
