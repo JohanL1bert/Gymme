8f08616d751a65148d7b7e2544a13968fc5b6d31
Johan <grimmerw968@gmail.com>
2023-03-01 13:56:35 +0200

    refactor: remove unused code

---
+++ src/stories/button/signUp.data.ts
@@ -0,0 +1,30 @@
+const btnSize = {
+  medium: 'px-[30px] py-[12px]',
+  large: 'px-[40px] py-[14px]',
+};
+
+const btnCol = {
+  white: 'bg-white',
+  green: 'bg-primary-200',
+  aqua: 'bg-aqua',
+  violet: 'bg-violet',
+  pink: 'bg-pink',
+  yellow: 'bg-yellow',
+};
+
+const btnBorder = {
+  none: 'border-none',
+  rounded: 'border-[1px] rounded-[1px]',
+};
+
+const textCol = {
+  white: 'text-white',
+  black: 'text-neutral-500',
+};
+
+export const buttonData = {
+  btnSize,
+  btnBorder,
+  textCol,
+  btnCol,
+};
