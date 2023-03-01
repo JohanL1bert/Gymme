d63de3d443f6bf19691c8eec0418829abedc22e2
Johan <grimmerw968@gmail.com>
2023-03-01 13:59:58 +0200

    feat: add color changer runtime in storybook app

--- src/stories/button/SignUp.tsx
+++ src/stories/button/SignUp.tsx
@@ -1,19 +1,17 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
 import classnames from 'classnames';
-import { TVisualBtn } from './SignUp.stories';
-import { buttonData } from './signUp.typography';
+import { buttonData } from './signUp.data';
 
 interface IBtnStyles {
-  /*   visualBtn: TVisualBtn; */
   size: 'medium' | 'large';
   text?: string;
-  bgColor: string;
+  bgColor: string | undefined;
+  backgroundColor?: any;
   textColor: 'white' | 'black';
   border: 'none' | 'rounded';
   children?: React.ReactNode;
 }
-
 const { btnSize, btnBorder, textCol, btnCol } = buttonData;
 
 export const SignUp = ({
@@ -24,13 +22,20 @@ export const SignUp = ({
   textColor = 'black',
   ...props
 }: IBtnStyles) => {
+  console.log(bgColor, 'bgColor');
   return (
     <button
       className={classnames(
         btnBorder[border],
         textCol[textColor],
-        btnCol[bgColor as keyof typeof btnCol]
+        /*  btnCol[bgColor as keyof typeof btnCol] */
+        bgColor
       )}
+      style={
+        (bgColor as any) && {
+          backgroundColor: bgColor,
+        }
+      }
     >
       <div className={classnames(btnSize[size])}>{text.toUpperCase()}</div>
     </button>
