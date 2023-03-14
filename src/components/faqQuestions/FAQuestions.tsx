2150ca9d74863ef07353b37746d72e448332f8e1
Johan <grimmerw968@gmail.com>
2023-03-14 16:03:49 +0200

    feat: add show hide block to FAQ section

--- src/components/faqQuestions/FAQuestions.tsx
+++ src/components/faqQuestions/FAQuestions.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import classNames from 'classnames';
 
 export const FAQuestions = ({
   heading,
@@ -9,16 +10,28 @@ export const FAQuestions = ({
   textInfoUpper: string;
   textInfoLower: string;
 }) => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  // closed - 68px open - 214px
+  // h-[calc(theme(spacing.56)-10px)]
+
   return (
-    <div>
-      <div className="h-6 w-[calc(theme(spacing.96)+theme(spacing.80)+4px)]">
+    <li className="w-[calc(theme(spacing.96)+theme(spacing.96))]  bg-primary-100">
+      <div className="flex justify-between p-[30px]">
         <h6>{heading}</h6>
-        <button>2</button>
+        <button type="button" onClick={() => setIsOpen(!isOpen)}>
+          2
+        </button>
       </div>
-      <div>
+      <div
+        className={classNames('drop_down_menu', {
+          'drop_down_menu--show': isOpen,
+          'drop_down_menu--hide': !isOpen,
+        })}
+      >
         <div>{textInfoUpper}</div>
         <div>{textInfoLower}</div>
       </div>
-    </div>
+    </li>
   );
 };
