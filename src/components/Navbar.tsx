7539308a5edec2328bb898fde8150fcf612dbbae
Johan <grimmerw968@gmail.com>
2023-04-17 14:36:20 +0300

    feat: add navbar template

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,37 +1,28 @@
 import React from 'react';
-import { SignUp } from './button/SignUp';
-import { signUpTypography } from './button/signUp.typography';
+import { SignUp } from '../stories/button/SignUp';
 import { Logo } from './logo/Logo';
+import { SignUpPrimary } from 'stories/button/SignUp.stories';
+
+const navBarText = ['Home', 'About', 'Workouts', 'Pricing', 'Community', 'FAQ'];
 
 export const Navbar = () => {
   return (
     <nav className="flex justify-evenly items-center h-20 bg-neutral-500">
       <Logo />
       <ul className="flex w-[calc(theme(spacing.96)+theme(spacing.36))] justify-between h-5 items-center relative z-10">
-        <li className="hover_navbar">
-          <a href="#">Home</a>
-        </li>
-        <li className="hover_navbar">
-          <a href="#">About</a>
-        </li>
-        <li className="hover_navbar">
-          <a href="#">Workouts</a>
-        </li>
-        <li className="hover_navbar">
-          <a href="">Pricing</a>
-        </li>
-        <li className="hover_navbar">
-          <a href="">Community</a>
-        </li>
-        <li className="hover_navbar">
-          <a href="">FAQ</a>
-        </li>
+        {navBarText.map((nav, index) => (
+          <li key={index} className="hover_navbar">
+            <a href="#" className="navbar_link">
+              {nav}
+            </a>
+          </li>
+        ))}
       </ul>
-      <div>
+      <div className="relative z-10">
         <button className="">
           <div className="uppercase py-3 px-7">Log in</div>
         </button>
-        <SignUp btnStyle={signUpTypography.medium.green} />
+        <SignUp {...SignUpPrimary.args} />
       </div>
     </nav>
   );
