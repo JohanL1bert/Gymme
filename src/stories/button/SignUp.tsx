2a84a67e77caba64e06e9832b69abce896094551
Johan <grimmerw968@gmail.com>
2023-07-10 18:17:58 +0300

    feat: add new values to change button style

--- src/stories/button/SignUp.tsx
+++ src/stories/button/SignUp.tsx
@@ -5,16 +5,20 @@ import { signUpButtonData } from './signUp.data';
 import { getValueFromKeysObject } from 'helpers/getValueFromKeysObject';
 
 interface IBtnStyles {
-  size?: 'medium' | 'large';
+  size?: 'small' | 'medium' | 'large';
   text?: string;
   bgColor?: string | undefined;
   backgroundColor?: any;
-  padding?: 'medium' | 'large';
+  padding?: 'medium' | 'large' | 'small';
   textColor?: 'white' | 'black';
   border?: 'none' | 'rounded';
+  fontStyle?: 'H1' | 'H2' | 'H3' | 'H4' | 'H5' | 'H6';
+  fontSize?: 'esm' | 'sm' | 'base' | 'lg' | 'xl';
+  lHeightValue?: 'lHSmall';
   children?: React.ReactNode;
 }
-const { btnPadding, btnBorder, textCol, btnCol } = signUpButtonData;
+const { btnPadding, btnBorder, textCol, btnCol, fStyle, fSize, lHeight } =
+  signUpButtonData;
 
 export const SignUp: React.FC<IBtnStyles> = ({
   size = 'medium',
@@ -22,6 +26,9 @@ export const SignUp: React.FC<IBtnStyles> = ({
   bgColor,
   border = 'none',
   textColor = 'black',
+  fontStyle = 'H1',
+  fontSize = 'sm',
+  lHeightValue = 'lHSmall',
   ...props
 }: IBtnStyles) => {
   return (
@@ -29,10 +36,21 @@ export const SignUp: React.FC<IBtnStyles> = ({
       className={classnames(
         btnBorder[border],
         textCol[textColor],
-        btnCol[bgColor as keyof typeof btnCol]
+        btnCol[bgColor as keyof typeof btnCol],
+        'h-full'
       )}
     >
-      <div className={classnames(btnPadding[size])}>{text.toUpperCase()}</div>
+      <div
+        className={classnames(
+          btnPadding[size],
+          fStyle[fontStyle],
+          fSize[fontSize],
+          lHeight[lHeightValue],
+          'my-auto'
+        )}
+      >
+        {text.toUpperCase()}
+      </div>
     </button>
   );
 };
--- src/stories/button/signUp.data.ts
+++ src/stories/button/signUp.data.ts
@@ -1,7 +1,8 @@
 const btnPadding = {
-  medium: 'px-[30px] py-[12px]',
-  large: 'px-[40px] py-[14px]',
-};
+  small: 'px-3 py-2',
+  medium: 'px-[calc(theme(spacing.8)-theme(spacing[0.5]))] py-3',
+  large: 'px-10 py-3.5',
+} as const;
 
 const btnCol = {
   white: 'bg-white',
@@ -10,21 +11,46 @@ const btnCol = {
   violet: 'bg-violet',
   pink: 'bg-pink',
   yellow: 'bg-yellow',
-};
+  transparent: 'bg-transparent',
+} as const;
 
 const btnBorder = {
   none: 'border-none',
   rounded: 'border-[1px] rounded-[1px]',
-};
+} as const;
 
 const textCol = {
   white: 'text-white',
   black: 'text-neutral-500',
-};
+} as const;
+
+const fStyle = {
+  H1: 'H1',
+  H2: 'H2',
+  H3: 'H3',
+  H4: 'H4',
+  H5: 'H5',
+  H6: 'H6',
+} as const;
+
+const fSize = {
+  esm: 'text-esm',
+  sm: 'text-sm',
+  base: 'text-base',
+  lg: 'text-lg',
+  xl: 'text-xl',
+} as const;
+
+const lHeight = {
+  lHSmall: 'leading-[18px]',
+} as const;
 
 export const signUpButtonData = {
   btnPadding,
   btnBorder,
   textCol,
   btnCol,
+  fStyle,
+  fSize,
+  lHeight,
 };
