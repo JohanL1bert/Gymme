d26480217dc14e25c4cd160bf55e011dbd241be9
Johan <grimmerw968@gmail.com>
2023-07-10 15:28:38 +0300

    feat: add response to question image, to block of text

--- src/components/FAQ.tsx
+++ src/components/FAQ.tsx
@@ -7,24 +7,26 @@ import { ReactComponent as VectorQuestionMark } from '../assets/icons/Vector.svg
 export const FAQ = () => {
   return (
     <section>
-      <div className="h-auto">
-        <div className="flex flex-col items-center gap-y-[85px] h-auto">
+      <div className="h-auto w-full">
+        <div className="flex flex-col items-center gap-y-[85px] mx-[calc(theme(spacing.28)-theme(spacing.6))]">
           <div className="faq_mark flex flex-col items-center">
-            <h2 className="H2 mt-[108px] mb-[77px]">
+            <h2 className="H2  mt-[calc(theme(spacing.28)-theme(spacing.1))] mb-[calc(theme(spacing.20)-theme(spacing.1))]">
               {'faq'.toUpperCase()}
               <span className="text-primary-200">.</span>
               <span />
             </h2>
-            <div>DELETE</div>
-            <div className="flex flex-col items-center">
-              <ul className="gap-y-[16px] flex flex-col">
+            <div className="flex flex-col items-center ">
+              <ul className="gap-y-[16px] flex flex-col ">
                 {faQuestionsData.map((item) => (
                   <FAQuestions key={item.id} {...item} />
                 ))}
               </ul>
             </div>
           </div>
-          <div className="faq_dot" />
+          {/* FIXME: Fix resize of dot */}
+          <div className="h-[248px] w-full flex justify-center items-center">
+            <div className="faq_dot" />
+          </div>
         </div>
       </div>
     </section>
