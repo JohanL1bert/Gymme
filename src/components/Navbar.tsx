66a3bbceb36d8dc98dfadddb09108ed5e6e80883
Johan <grimmerw968@gmail.com>
2023-10-16 18:43:41 +0300

    feat: add half responsive to navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,8 +2,7 @@ import React from 'react';
 import { SignUp } from '../stories/button/SignUp';
 import { Logo } from './logo/Logo';
 import { SignUpPrimary, LogInSmall } from 'stories/button/SignUp.stories';
-
-const navBarText = ['Home', 'About', 'Workouts', 'Pricing', 'Community', 'FAQ'];
+import { navBarText } from 'helpers/navBarInfo';
 
 export const Navbar = () => {
   return (
@@ -11,9 +10,16 @@ export const Navbar = () => {
       <div className="px-[theme(spacing.20)] py-5 w-full h-full flex">
         <nav className="flex justify-between w-full max-w-[1980px] items-center h-[calc(theme(spacing.10)+2px)]  shrink-0">
           <Logo />
+
+          <div className="iPad:hidden">
+            <div className="w-[30px] h-[3px] mt-[5px] bg-white transition-all ease-in-out duration-300" />
+            <div className="w-[30px] h-[3px] mt-[5px] bg-white transition-all ease-in-out duration-300" />
+            <div className="w-[30px] h-[3px] mt-[5px] bg-white transition-all ease-in-out duration-300" />
+          </div>
+
           <ul className="flex w-[calc(theme(spacing.96)+theme(spacing.36))] justify-between h-5 items-center relative z-10">
-            {navBarText.map((nav, index) => (
-              <li key={index} className="hover_navbar">
+            {navBarText.map(({ nav, id }) => (
+              <li key={id} className="hover_navbar">
                 <a href="#" className="navbar_link">
                   {nav}
                 </a>
