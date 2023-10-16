0ee1a13c083a42f324f392a57ca4779a8baf876f
Johan <grimmerw968@gmail.com>
2023-10-16 18:42:58 +0300

    feat: add navBarInfo

---
+++ src/helpers/navBarInfo.ts
@@ -0,0 +1,26 @@
+export const navBarText = [
+  {
+    id: 1,
+    nav: 'Home',
+  },
+  {
+    id: 2,
+    nav: 'About',
+  },
+  {
+    id: 3,
+    nav: 'Workouts',
+  },
+  {
+    id: 4,
+    nav: 'Pricing',
+  },
+  {
+    id: 5,
+    nav: 'Community',
+  },
+  {
+    id: 6,
+    nav: 'FAQ',
+  },
+];
