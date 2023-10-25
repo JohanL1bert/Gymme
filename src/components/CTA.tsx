42b70a165d879ecc59e2e63b5badf89026fc1b6a
Johan <grimmerw968@gmail.com>
2023-10-25 18:24:44 +0300

    feat: add responsive to all view for CTA page

--- src/components/CTA.tsx
+++ src/components/CTA.tsx
@@ -9,18 +9,23 @@ export const CTA = () => {
   // first div height iPad:max-h-[calc(theme(spacing.96)+theme(spacing.40)-7px)]
   return (
     <section>
-      <div className=" h-full bg-neutral-500 flex iPad:justify-center iPad:items-center iPad:flex-row mobile:flex-col">
+      <div
+        className=" h-full bg-neutral-500 flex iPad:justify-center iPad:items-center iPad:flex-row mobile:flex-col
+      mobileSmall:flex-col"
+      >
         <div className="h-full ">
           <img
             src={CTAImg}
             alt="image of CTA"
-            className="max-h-[618px] h-full -translate-y-10 iPad:max-w-[500px] mobile:max-w-[calc(100%-276px)] mobile:min-w-[276px]"
+            className="max-h-[618px] h-full -translate-y-10 iPad:max-w-[500px] mobile:max-w-[calc(100%-276px)] mobile:min-w-[276px]
+            mobileSmall:min-w-[270px] mobileSmall:max-w-[270px]"
           />
         </div>
         <div
           className="iPad:max-h-[290px] iPad:h-full iPad:max-w-[492px] iPad:ml-0 w-full iPad:gap-0 iPad:mb-0 flex flex-col iPad:-translate-x-12 items-start
         mobile:translate-x-0 mobile:max-h-[236px] mobile:max-w-[calc(100%-348px)] mobile:min-w-[348px] mobile:gap-4
-        mobile:ml-[30px] mobile:mb-[50px] mobile:-translate-y-20"
+        mobile:ml-[30px]  mobile:-translate-y-20
+        mobileSmall:-translate-y-20 mobileSmall:gap-4 mobileSmall:ml-[10px] mobileSmall:max-w-[250px] mobileSmall:max-h-[236px]"
         >
           <div>
             <h3
