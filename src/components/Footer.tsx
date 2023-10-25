035969e6d171a2ce2bfb2e1a72120b0318faf9bf
Johan <grimmerw968@gmail.com>
2023-10-25 18:26:28 +0300

    feat: add responsive view to footer page

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,11 @@ import { Logo } from './logo/Logo';
 export const Footer = () => {
   return (
     <footer>
-      <div className="h-[calc(theme(spacing.48)+3px)] bg-neutral-500 flex items-center iPad:justify-between iPad:px-16 mobile:px-7 mobileSmall:px-1">
+      <div
+        className="iPad:h-[calc(theme(spacing.48)+3px)] bg-neutral-500 flex items-center iPad:justify-between iPad:px-16
+      mobile:px-7 mobile:justify-between mobile:h-28
+      mobileSmall:px-1 mobileSmall:justify-between"
+      >
         <Logo />
         <div className="text-neutral-300 h-[calc(theme(spacing.4)+2px)] footer_text">
           All rights reserved. Gymme
