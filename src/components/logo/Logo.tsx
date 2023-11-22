2ffcde9c19b635f96c3d0686da089dc1343f5722
Johan <grimmerw968@gmail.com>
2023-11-22 17:51:00 +0200

    fix: logo transparent

--- src/components/logo/Logo.tsx
+++ src/components/logo/Logo.tsx
@@ -3,7 +3,7 @@ import { ReactComponent as LogoSvg } from '../../assets/icons/Logo.svg';
 
 export const Logo = () => {
   return (
-    <div className="flex w-[calc(theme(spacing.20)+45px)] h-6 justify-center items-center bg-neutral-500 ">
+    <div className="flex w-[calc(theme(spacing.20)+45px)] h-6 justify-center items-center bg-[transparent] ">
       <LogoSvg />
     </div>
   );
