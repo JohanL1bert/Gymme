ccea4e4c20d3d0f1253006e45789b29a3dbd8552
Johan <grimmerw968@gmail.com>
2023-11-23 20:39:12 +0200

    fix: layouts, change structure of elements, add svg

--- src/components/FAQ.tsx
+++ src/components/FAQ.tsx
@@ -1,44 +1,39 @@
 import React from 'react';
 import { faQuestionsData } from './faqQuestions/faQuestions.data';
 import { FAQuestions } from './faqQuestions/FAQuestions';
-/* import { ReactComponent as VectorDot } from '../assets/icons/Vector1.svg';
-import { ReactComponent as VectorQuestionMark } from '../assets/icons/Vector.svg'; */
-
-// max-h-[calc(theme(spacing.96)+theme(spacing.96)+theme(spacing.96)+5px)]
+import { ReactComponent as VectorDot } from '../assets/icons/Vector1.svg';
+import { ReactComponent as VectorQuestionMark } from '../assets/icons/Vector.svg';
 
 export const FAQ = () => {
   return (
     <section>
-      <div className="w-full min-h-full h-full mb-20">
-        <div className="max-w-full relative">
-          <div className="faq">
-            <div className="faq_mark" />
-            <div className=" w-[calc(100%/3)] h-[calc(theme(spacing.60)+4px)] flex justify-center items-center ">
-              <div className="faq_dot" />
+      <div className="w-full  h-full mb-20 margin_top_section min-h-[1157.5px] flex flex-col justify-start relative">
+        <div className="flex flex-col items-center gap-20 faq">
+          <VectorQuestionMark />
+          <div className="w-full grid grid-cols-3 justify-items-center">
+            <div className="" />
+            <div className=" -translate-x-5 max-w-[252px] w-full max-h-[248px] flex-grow shrink">
+              <VectorDot />
             </div>
+            <div className="" />
           </div>
         </div>
-
-        <div>
+        <div className="">
           <div
-            className="flex flex-col items-center gap-y-[85px] mx-[calc(theme(spacing.28)-theme(spacing.6))]
-        min-h-[calc(theme(spacing.96)+theme(spacing.96)+theme(spacing.96)+5px)] h-full
-        mobile:mx-28
-        mobileSmall:mx-5"
+            className="flex flex-col items-center  gap-y-[85px] mobile:mx-[calc(100vw-95%)] h-full
+  mobileSmall:mx-5 mt-[108px]"
           >
-            <div className="flex flex-col items-center h-full">
-              <h2 className="H2 pt-24 -z-10 absolute">
-                {'faq'.toUpperCase()}
-                <span className="text-primary-200">.</span>
-                <span />
-              </h2>
-              <div className="flex flex-col items-center mt-[calc(theme(spacing.56))]  ">
-                <ul className="gap-y-[16px] flex flex-col ">
-                  {faQuestionsData.map((item) => (
-                    <FAQuestions key={item.id} {...item} />
-                  ))}
-                </ul>
-              </div>
+            <h2 className="H2 ">
+              {'faq'.toUpperCase()}
+              <span className="text-primary-200">.</span>
+              <span />
+            </h2>
+            <div className="flex flex-col items-center ">
+              <ul className="gap-y-[16px] flex flex-col ">
+                {faQuestionsData.map((item) => (
+                  <FAQuestions key={item.id} {...item} />
+                ))}
+              </ul>
             </div>
           </div>
         </div>
